524323f5eaf2051a1e685ca8c0d85f7790ab5939
asmi046 <asmi046@gmail.com>
2024-10-30 16:48:30 +0300

    Бд

--- database/migrations/2024_10_28_082345_create_tours_table.php
+++ database/migrations/2024_10_28_082345_create_tours_table.php
@@ -18,7 +18,8 @@ return new class extends Migration
             $table->string('slug')->unique()->comment('Слаг');
             $table->string('img', 500)->nullable()->comment('Изображение');
             $table->integer('deycount')->default(1)->comment('Количество дней');
-            $table->date('start_data')->default(DB::raw('CURRENT_TIMESTAMP'))->comment('Дата поездки');
+            // $table->date('start_data')->default(DB::raw('CURRENT_TIMESTAMP'))->comment('Дата поездки');
+            $table->date('start_data')->comment('Дата поездки');
             $table->mediumText('top_description')->comment('Верхнее описание');
             $table->mediumText('program')->nullable()->comment('Программа тура');
             $table->mediumText('in_price')->comment('Входит в стоимость');
